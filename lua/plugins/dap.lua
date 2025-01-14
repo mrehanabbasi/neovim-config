bc565e21c01f7440447bf091749e8bdf1810756a
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2025-01-14 11:45:26 +0200

    update dap keymap to use keys field

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,29 +1,18 @@
 return {
   {
     "mfussenegger/nvim-dap",
-    config = function()
-      require("which-key").add {
-        {
-          mode = "n",
-          { "<leader>db", "<cmd> DapToggleBreakpoint <CR>", desc = "Add breakpoint at line", icon = "" },
-          {
-            "<leader>dus",
-            function()
-              local widgets = require "dap.ui.widgets"
-              local sidebar = widgets.sidebar(widgets.scopes)
-              sidebar.open()
-            end,
-            desc = "Open debugging sidebar",
-          },
-        },
-      }
-      -- vim.keymap.set("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Add breakpoint at line" })
-      -- vim.keymap.set("n", "<leader>dus", function()
-      --   local widgets = require "dap.ui.widgets"
-      --   local sidebar = widgets.sidebar(widgets.scopes)
-      --   sidebar.open()
-      -- end, { desc = "Open debugging sidebar" })
-    end,
+    keys = {
+      { "<leader>db", "<cmd> DapToggleBreakpoint <CR>", desc = "Add breakpoint at line" },
+      {
+        "<leader>dus",
+        function()
+          local widgets = require("dap.ui.widgets")
+          local sidebar = widgets.sidebar(widgets.scopes)
+          sidebar.open()
+        end,
+        desc = "Open debugging sidebar",
+      },
+    },
   },
   {
     "leoluz/nvim-dap-go",
@@ -31,7 +20,7 @@ return {
     dependencies = "mfussenegger/nvim-dap",
     config = function(_, opts)
       require("dap-go").setup(opts)
-      require("which-key").add {
+      require("which-key").add({
         {
           mode = "n",
           {
@@ -49,7 +38,7 @@ return {
             desc = "Debug last go test",
           },
         },
-      }
+      })
       -- vim.keymap.set("n", "<leader>dgt", function()
       --   require("dap-go").debug_test()
       -- end, { desc = "Debug go test" })

33778988f591da66c80981c8009f9011159e019f
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2025-03-24 13:20:34 +0200

    enable nvim.core

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,20 +1,5 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    keys = {
-      { "<leader>db", "<cmd> DapToggleBreakpoint <CR>", desc = "Add breakpoint at line" },
-      {
-        "<leader>dus",
-        function()
-          local widgets = require("dap.ui.widgets")
-          local sidebar = widgets.sidebar(widgets.scopes)
-          sidebar.open()
-        end,
-        desc = "Open debugging sidebar",
-      },
-    },
-  },
-  {
     "leoluz/nvim-dap-go",
     ft = "go",
     dependencies = "mfussenegger/nvim-dap",
